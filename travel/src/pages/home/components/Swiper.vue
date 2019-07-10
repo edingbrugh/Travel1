c07ae1525a7bd410dd46b010e5c05899c8fb39bd
edingbrugh <1924672031@qq.com>
2019-07-10 22:41:13 +0800

    添加了swiper

---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl"  alt="">
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper-slide>
+  </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {id: '01',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/29/2a6b77b6ff72402.jpg_750x200_d4cec568.jpg'
+        },
+        {
+          id: '02',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/29/2a6b77b6ff72402.jpg_750x200_d4cec568.jpg'
+        },
+        {
+          id: '03',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/29/2a6b77b6ff72402.jpg_750x200_d4cec568.jpg'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background:#ffffff
+  .wrapper
+    height :0
+    width :100%
+    overflow :hidden
+    padding-bottom :31.25%
+    .swiper-img
+      width 100%
+</style>
